73008cd9492981dcdeb8925229a5841faf509d80
Smallten <smallten@smallten.me>
2026-02-09 16:34:44 +0800

    feat: init header

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,52 @@
+<script setup lang="ts">
+import type { NavigationMenuItem } from '@nuxt/ui'
+
+const route = useRoute()
+const menuItems = computed<NavigationMenuItem[]>(() => [
+  { label: '首頁', to: '/' },
+  { label: '文章', to: '/articles', active: route.path.startsWith('/articles') },
+])
+</script>
+
 <template>
   <UApp>
-    <UHeader />
+    <UHeader>
+      <template #title>
+        <img src="/logo2.webp" class="h-12" alt="Logo" aria-label="Logo" />
+      </template>
+
+      <UNavigationMenu
+        :items="menuItems"
+        variant="link"
+        :ui="{
+          link: 'text-lg',
+        }"
+      />
+
+      <template #right>
+        <UColorModeButton size="xl" />
+        <UButton
+          color="neutral"
+          variant="ghost"
+          to="https://github.com/st-avi"
+          target="_blank"
+          icon="i-simple-icons-github"
+          aria-label="GitHub"
+          size="xl"
+        />
+      </template>
+
+      <template #body>
+        <UNavigationMenu
+          :items="menuItems"
+          orientation="vertical"
+          class="-mx-2.5"
+          :ui="{
+            link: 'text-lg',
+          }"
+        />
+      </template>
+    </UHeader>
 
     <UMain>
       <NuxtLayout>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,4 +9,7 @@ export default defineNuxtConfig({
     plugins: [tailwindcss() as any],
   },
   modules: ['@nuxt/eslint', '@nuxt/ui'],
+  icon: {
+    serverBundle: 'remote',
+  },
 })
