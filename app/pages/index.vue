d6bbfa4302f85e180ee9c1d8651ca5ee3c00f1f7
Smallten <smallten@smallten.me>
2026-02-10 11:33:57 +0800

    feat: add CTA to homepage

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,32 @@
+<script setup lang="ts">
+import type { ButtonProps } from '@nuxt/ui'
+
+const ctaLinks = ref<ButtonProps[]>([
+  { label: '看文章', color: 'neutral', variant: 'subtle', trailingIcon: 'i-lucide-arrow-right' },
+])
+</script>
+
 <template>
   <UPage>
-    <div>Home</div>
+    <UPageCTA
+      title="夢想起飛的學習基地"
+      description="知識起航，未來翱翔"
+      variant="soft"
+      orientation="horizontal"
+      reverse
+      :links="ctaLinks"
+      class="justify-center"
+      :ui="{
+        container: 'lg:grid-cols-3',
+        wrapper: 'lg:col-span-2',
+        title: 'text-center',
+        description: 'text-center',
+        footer: 'flex justify-around',
+      }"
+    >
+      <div class="flex w-full justify-center lg:justify-end">
+        <img src="/logo1.webp" class="size-30 sm:size-40" alt="logo" aria-label="logo" />
+      </div>
+    </UPageCTA>
   </UPage>
 </template>
